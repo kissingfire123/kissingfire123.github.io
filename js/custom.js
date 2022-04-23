bd4045530d5d5cbd53fbc7464dbc095ee3467d6b
kissingfire123 <lwy13697320205@163.com>
2022-04-23 23:18:46 +0800

    完善Effective-C++(4),Rule43

--- js/custom.js
+++ js/custom.js
@@ -13,7 +13,7 @@ function runtime() {
     let leavel3 = leavel2 % (60 * 1000);
     let seconds = Math.floor(leavel3 / (1000));
     let runbox = document.getElementById('run-time');
-    runbox.innerHTML = '本站已运行<i class="far fa-clock fa-fw"></i> '
+    runbox.innerHTML = '本站已运行<i class="far fa-solid fa-heart-pulse"></i> '
         + ((days < 10) ? '0' : '') + days + ' 天 '
         + ((hours < 10) ? '0' : '') + hours + ' 时 '
         + ((minutes < 10) ? '0' : '') + minutes + ' 分 '
